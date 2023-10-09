1d174f52038c09d3503929c15ac7c625b225f72c
Kuleii <147216070+Kuleii@users.noreply.github.com>
2023-10-09 14:51:01 -0600

    mejorar script escorpion

--- Assets/Scripts/escorpion.cs
+++ Assets/Scripts/escorpion.cs
@@ -10,18 +10,22 @@ public class escorpion : MonoBehaviour
 {
     public Transform playerPosition;
     public float speed;
+    public Rigidbody2D hitbox;
+    public Vector2 goPosition;
+    public Vector3 offset=new Vector3(0,0.72f,0);
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hitbox=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         playerPosition=GameObject.FindWithTag("Player").transform;
-        gameObject.transform.position=Vector2.MoveTowards(gameObject.transform.position,playerPosition.position,speed*Time.deltaTime);
+        goPosition=Vector2.MoveTowards(gameObject.transform.position,playerPosition.position-offset,speed*Time.deltaTime);
+        hitbox.MovePosition(goPosition);
     }
 }
